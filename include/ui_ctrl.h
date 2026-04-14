9c0dd145e7fe9421d79f5c4919a167130bb6ae32
HeZzz <hez2z@foxmail.com>
2026-04-14 22:02:45 +0800

    refactor(layout): 清理旧平铺源码路径

--- include/ui_ctrl.h
+++
@@ -1,7 +0,0 @@
-#ifndef UI_CTRL_H
-#define UI_CTRL_H
-
-void ui_ctrl_init(void);
-void ui_ctrl_poll(void);
-
-#endif
--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include "app.h"
-
-int main(void) {
-  app_init();
-
-  while (1) {
-    app_run_once();
-  }
-}
